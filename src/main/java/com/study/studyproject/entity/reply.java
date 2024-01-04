1f051aac89f6a78248ca48f1c4a6e3608db4bf25
jacomyou1026 <jacomyou1026@naver.com>
2024-01-04 22:25:11 +0900

    feat  : 답글 entity 생성과 생성자와 삭제 메서드 (#1)

--- src/main/java/com/study/studyproject/entity/reply.java
+++ src/main/java/com/study/studyproject/entity/reply.java
@@ -1,10 +1,11 @@
 package com.study.studyproject.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
@@ -14,7 +15,42 @@ public class reply extends BaseTimeEntity{
     @Id
     @GeneratedValue
     private Long id;
+
+    private String nickname;
     private String content;
+    private String ip;
+
+    private int depth; // 원글 0 : 댓글 :1
+    private int ref; // 부모 ip 참조
+    private boolean is_delete; // 삭제여뷰
+    private LocalDateTime delete_at; // 삭제시간
+
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+
+    @Builder
+    public reply(String nickname, String content, String ip, int depth, int ref,  Member user, Board board) {
+        this.nickname = nickname;
+        this.content = content;
+        this.ip = ip;
+        this.depth = 0;
+        this.ref = 1;
+        this.is_delete = false;
+        this.user = user;
+        this.board = board;
+    }
+
+    public void deleteReply() {
+        this.is_delete = true;
+    }
 
 
 }
