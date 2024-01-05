5df22fff06f6c3f278c7ac173355d02222867c58
jacomyou1026 <jacomyou1026@naver.com>
2024-01-05 20:47:37 +0900

    feat (JwtFilter): JwtFilter  구현 (#4)

---
+++ src/main/java/com/study/studyproject/global/jwt/JwtFilter.java
@@ -0,0 +1,59 @@
+package com.study.studyproject.global.jwt;
+
+import com.study.studyproject.global.exception.ex.jwtException;
+import io.jsonwebtoken.Jwt;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+
+    private final JwtUtil jwtUtil;
+
+    @SneakyThrows
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String accessToken = jwtUtil.getHeaderToken(request, JwtUtil.ACCESS_TOKEN);
+        String refreshToken = jwtUtil.getHeaderToken(request, JwtUtil.REFRESH_TOKEN);
+
+        if (jwtUtil.tokenValidation(accessToken)) {
+            setAuthentication(jwtUtil.getEmailFromToken(accessToken));
+        } else if (refreshToken != null) {
+            Boolean isRefreshToken = jwtUtil.refreshTokenValidation(refreshToken);
+
+            if (isRefreshToken) {
+                String loginId = jwtUtil.getEmailFromToken(refreshToken);
+                String newAccessToken = jwtUtil.createToken(loginId, JwtUtil.ACCESS_TOKEN);
+                jwtUtil.setHeaderAccessToken(response, newAccessToken);
+                setAuthentication(jwtUtil.getEmailFromToken(newAccessToken));
+            } else {
+                throw new jwtException("RefreshToken Expired");
+            }
+
+        }
+
+
+        filterChain.doFilter(request, response);
+
+
+
+    }
+
+    private void setAuthentication(String email) {
+        Authentication authentication = jwtUtil.createAuthentication(email);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+}
