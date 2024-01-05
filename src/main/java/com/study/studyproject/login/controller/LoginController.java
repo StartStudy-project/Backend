351c813f6167f4d401d3e264f3fe15929987c704
jacomyou1026 <jacomyou1026@naver.com>
2024-01-05 23:12:24 +0900

    refactor (LoginService):  중복확인 회원가입 구현 (#4)

--- src/main/java/com/study/studyproject/login/controller/LoginController.java
+++ src/main/java/com/study/studyproject/login/controller/LoginController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
@@ -20,9 +21,10 @@ public class LoginController {
 
     private final LoginService loginService;
 
+    //회원가입
     @PostMapping("/sign")
     public ResponseEntity<GlobalResultDto> sign(@Validated @RequestBody com.study.studyproject.login.dto.SignRequest signRequest) {
-        return ResponseEntity.created(URI.create("/login")).body(loginService.sign(signRequest));
+        return ResponseEntity.ok(loginService.sign(signRequest));
 
     }
 
@@ -30,7 +32,6 @@ public class LoginController {
 
     @PostMapping("/login")
     public ResponseEntity<GlobalResultDto> login(@Validated @RequestBody LoginRequest loginRequest, HttpServletResponse response) {
-
         return ResponseEntity.ok(loginService.loginService(loginRequest, response));
 
 
