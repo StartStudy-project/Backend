8153ea88fe1992efe44a43b231a88183ff14d014
jacomyou1026 <jacomyou1026@naver.com>
2024-01-05 18:35:56 +0900

    refactor (CorsConfig): 패키징 리펙토링   (#4)

--- src/main/java/com/study/studyproject/global/config/CorsConfig.java
+++ src/main/java/com/study/studyproject/global/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.study.studyproject.login.config;
+package com.study.studyproject.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
