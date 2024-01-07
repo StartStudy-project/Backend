ffc1f511c177fb65a4f77477833136f5802e3a93
jacomyou1026 <jacomyou1026@naver.com>
2024-01-07 13:57:02 +0900

    feat (StudyController) :  study controller 기능 구현 (#4)

--- src/main/java/com/study/studyproject/study/controller/StudyController.java
+++ src/main/java/com/study/studyproject/study/controller/StudyController.java
@@ -1,26 +1,26 @@
-//package com.study.studyproject.study.controller;
-//
-//import com.study.studyproject.study.service.StudyService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequiredArgsConstructor
-//public class StudyController {
-//
-////    @Autowired
-////    private final StudyService studyService;
-//
-//    @GetMapping("/main/${type}")
-//    public String studyMain(
-//            @PathVariable(name = "type", required = false) String type,
-//            @RequestParam(defaultValue = "0", required = false) int seq,
-//            @RequestParam(defaultValue="0", required =false) int page
-//
-//    ) {
-////        studyService.studyMainService(type,seq,page);
-//        return "";
-//
-//    }
-//}
+package com.study.studyproject.study.controller;
+
+import com.study.studyproject.study.service.StudyService;
+import com.study.studyproject.study.service.StudyServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class StudyController {
+
+    private  final StudyServiceImpl studyService;
+
+    @GetMapping("/main/${type}")
+    public String studyMain(
+            @PathVariable(name = "type", required = false) String type,
+            @RequestParam(defaultValue = "seq", required = false) String order,
+            @RequestParam(defaultValue="0", required =false) int page
+
+    ) {
+        studyService.selectBoardList(type,order,page);
+        return "";
+
+    }
+}
