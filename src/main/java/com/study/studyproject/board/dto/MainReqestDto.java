74a2a2a7754f939953311893cadc9d65b7e4f617
jacomyou1026 <jacomyou1026@naver.com>
2024-01-07 19:36:59 +0900

    feat  () : MainResponset ,request 기능 구현 (#8)

---
+++ src/main/java/com/study/studyproject/board/dto/MainReqestDto.java
@@ -0,0 +1,11 @@
+package com.study.studyproject.board.dto;
+
+import lombok.Data;
+
+@Data
+public class MainReqestDto {
+
+    private String type;
+    private String order;
+
+}
---
+++ src/main/java/com/study/studyproject/board/dto/MainResponsetDto.java
@@ -0,0 +1,14 @@
+package com.study.studyproject.board.dto;
+
+public class MainResponsetDto {
+    String type; //전체 코테
+    String order; // 최신 순
+    String recrit ; //모집여부
+    String title;
+    String content;
+    String time;
+    String writingId;
+    int hitCnt;
+    int replyCnt;
+
+}
