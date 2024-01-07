746c1127fd45944b43bd2adb17b6bd68d41a34e1
jacomyou1026 <jacomyou1026@naver.com>
2024-01-07 19:36:09 +0900

    refactor  () : restController로 수정 (#8)

--- src/main/java/com/study/studyproject/login/controller/LoginController.java
+++ src/main/java/com/study/studyproject/login/controller/LoginController.java
@@ -10,12 +10,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class LoginController {
 
