6687320b141e97bf349f0c3a10b4e3267813e174
jacomyou1026 <jacomyou1026@naver.com>
2024-01-20 23:38:42 +0900

    chro : ddl update

--- src/main/java/com/study/studyproject/global/config/SpringSecurity.java
+++ src/main/java/com/study/studyproject/global/config/SpringSecurity.java
@@ -43,7 +43,7 @@ public class SpringSecurity {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.addFilterBefore(corsFilter,UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(corsFilter,UsernamePasswordAuthenticationFilter.class);
         http.addFilterBefore(new JwtFilter(jwtUtil), CorsFilter.class);
         http.csrf(cs -> cs.disable()) //
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 생성x
