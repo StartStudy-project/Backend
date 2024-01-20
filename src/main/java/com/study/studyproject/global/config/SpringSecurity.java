ea6cf459fb9c3a50b20e6b4c40cc4b051737bda4
jacomyou1026 <jacomyou1026@naver.com>
2024-01-20 23:44:45 +0900

    chro : ddl update

--- src/main/java/com/study/studyproject/global/config/SpringSecurity.java
+++ src/main/java/com/study/studyproject/global/config/SpringSecurity.java
@@ -14,6 +14,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -31,7 +33,6 @@ public class SpringSecurity {
     }
 
     private final CorsFilter corsFilter;
-
 
     //제외될 url
     @Bean
