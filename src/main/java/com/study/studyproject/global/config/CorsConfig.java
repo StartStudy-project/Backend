647a6f87d9af7b69041d054ac8686aa6817b516b
jacomyou1026 <jacomyou1026@naver.com>
2024-01-24 00:16:58 +0900

    cicd : 오류 fix

--- src/main/java/com/study/studyproject/global/config/CorsConfig.java
+++ src/main/java/com/study/studyproject/global/config/CorsConfig.java
@@ -6,8 +6,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-//CORS : 다른 도메인에서 리소스에 접근할 때 브라우저에서 적용되는 보안 정책을 관리하는 메커니즘입니다
-// CORS (Cross-Origin Resource Sharing) 설정을 구현
 @Configuration
 public class CorsConfig {
 
