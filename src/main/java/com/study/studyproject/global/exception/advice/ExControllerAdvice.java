304e56307473581ece26643f1b2a7b8ef4811426
jacomyou1026 <jacomyou1026@naver.com>
2024-01-25 20:57:57 +0900

    cicd : 예외처리부분 뺴기

--- src/main/java/com/study/studyproject/global/exception/advice/ExControllerAdvice.java
+++ src/main/java/com/study/studyproject/global/exception/advice/ExControllerAdvice.java
@@ -1,126 +1,126 @@
-package com.study.studyproject.global.exception.advice;
-
-import com.study.studyproject.global.exception.ExceptionResponse;
-import com.study.studyproject.global.exception.ex.JwtException;
-import com.study.studyproject.global.exception.ex.TokenNotValidatException;
-import com.study.studyproject.global.exception.ex.UserNotFoundException;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.crossstore.ChangeSetPersister;
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpClientErrorException.Forbidden;
-import org.springframework.web.client.HttpClientErrorException.Unauthorized;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import javax.naming.AuthenticationException;
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import static org.springframework.http.HttpStatus.FORBIDDEN;
-
-@Slf4j
-@RestControllerAdvice
-public class ExControllerAdvice extends ResponseEntityExceptionHandler {
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST) //400
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ExceptionResponse illegalExHandler(IllegalArgumentException e) {
-        return new ExceptionResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
-    }
-
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<ExceptionResponse> userExHandler(UserNotFoundException e) {
-        ExceptionResponse errorResult = new ExceptionResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
-        return new ResponseEntity<>(errorResult, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ExceptionResponse> exhandle(JwtException e) {
-        ExceptionResponse errorResult = new ExceptionResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
-        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ExceptionResponse> tokenNotValidatException(TokenNotValidatException e) {
-        ExceptionResponse errorResult = new ExceptionResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
-        return new ResponseEntity<>(errorResult, HttpStatus.NOT_FOUND);
-    }
-
-
-    //500
-//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-//    @ExceptionHandler
-//    public ResponseEntity<ExceptionResponse> exception(Exception e) {
-//        ExceptionResponse errorResult = new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+//package com.study.studyproject.global.exception.advice;
+//
+//import com.study.studyproject.global.exception.ExceptionResponse;
+//import com.study.studyproject.global.exception.ex.JwtException;
+//import com.study.studyproject.global.exception.ex.TokenNotValidatException;
+//import com.study.studyproject.global.exception.ex.UserNotFoundException;
+//import jakarta.servlet.http.HttpServletResponse;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.data.crossstore.ChangeSetPersister;
+//import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.HttpStatusCode;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.access.AccessDeniedException;
+//import org.springframework.validation.FieldError;
+//import org.springframework.web.bind.MethodArgumentNotValidException;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseStatus;
+//import org.springframework.web.bind.annotation.RestControllerAdvice;
+//import org.springframework.web.client.HttpClientErrorException;
+//import org.springframework.web.client.HttpClientErrorException.Forbidden;
+//import org.springframework.web.client.HttpClientErrorException.Unauthorized;
+//import org.springframework.web.context.request.WebRequest;
+//import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+//
+//import javax.naming.AuthenticationException;
+//import java.time.LocalDateTime;
+//import java.util.HashMap;
+//import java.util.LinkedHashMap;
+//import java.util.Map;
+//
+//import static org.springframework.http.HttpStatus.FORBIDDEN;
+//
+//@Slf4j
+//@RestControllerAdvice
+//public class ExControllerAdvice extends ResponseEntityExceptionHandler {
+//
+//    @ResponseStatus(HttpStatus.BAD_REQUEST) //400
+//    @ExceptionHandler(IllegalArgumentException.class)
+//    public ExceptionResponse illegalExHandler(IllegalArgumentException e) {
+//        return new ExceptionResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+//    }
+//
+//    @ExceptionHandler(UserNotFoundException.class)
+//    public ResponseEntity<ExceptionResponse> userExHandler(UserNotFoundException e) {
+//        ExceptionResponse errorResult = new ExceptionResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
 //        return new ResponseEntity<>(errorResult, HttpStatus.NOT_FOUND);
 //    }
-
-
-    @ResponseStatus(FORBIDDEN)
-    @ExceptionHandler(value = { AccessDeniedException.class })
-    @ResponseBody
-    protected ResponseEntity forbidden(RuntimeException ex, WebRequest request) {
-        ExceptionResponse errorResult = new ExceptionResponse(FORBIDDEN.value(),ex.getMessage());
-        HashMap<String, String> stringStringHashMap = new HashMap<>();
-        stringStringHashMap.put("request.getParameterMap() : ", request.getParameterMap().toString());
-        stringStringHashMap.put("request.getContextPath() :  ", request.getContextPath());
-        stringStringHashMap.put("request.getHeaderNames().toString()) : ", request.getHeaderNames().toString());
-        stringStringHashMap.put("ex.toString() : ", ex.toString());
-        stringStringHashMap.put("ex.getMessage() : ", ex.getMessage());
-        return new ResponseEntity<>(stringStringHashMap, HttpStatus.FORBIDDEN);
-
-    }
-
-
-
-
-    @ExceptionHandler
-    public ResponseEntity<ExceptionResponse> exhandle(NotFoundException e) {
-        ExceptionResponse errorResult = new ExceptionResponse(HttpStatus.NOT_FOUND.value(),e.getMessage());
-        return new ResponseEntity<>(errorResult, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ExceptionResponse> exhandle(IllegalStateException e) {
-        ExceptionResponse errorResult = new ExceptionResponse(HttpStatus.BAD_REQUEST.value(),e.getMessage());
-        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
-    }
-
-
-    // Bean
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-                                                                  HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("status", status.value());
-        body.put("message", "값을 제대로 입력해주세요");
-
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        //
-        log.info("Errors", errors);
-        return new ResponseEntity<>(body, headers, status);
-    }
-
-
-
-}
+//
+//    @ExceptionHandler
+//    public ResponseEntity<ExceptionResponse> exhandle(JwtException e) {
+//        ExceptionResponse errorResult = new ExceptionResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+//        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
+//    }
+//
+//    @ExceptionHandler
+//    public ResponseEntity<ExceptionResponse> tokenNotValidatException(TokenNotValidatException e) {
+//        ExceptionResponse errorResult = new ExceptionResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
+//        return new ResponseEntity<>(errorResult, HttpStatus.NOT_FOUND);
+//    }
+//
+//
+//    //500
+////    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+////    @ExceptionHandler
+////    public ResponseEntity<ExceptionResponse> exception(Exception e) {
+////        ExceptionResponse errorResult = new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+////        return new ResponseEntity<>(errorResult, HttpStatus.NOT_FOUND);
+////    }
+//
+//
+//    @ResponseStatus(FORBIDDEN)
+//    @ExceptionHandler(value = { AccessDeniedException.class })
+//    @ResponseBody
+//    protected ResponseEntity forbidden(RuntimeException ex, WebRequest request) {
+//        ExceptionResponse errorResult = new ExceptionResponse(FORBIDDEN.value(),ex.getMessage());
+//        HashMap<String, String> stringStringHashMap = new HashMap<>();
+//        stringStringHashMap.put("request.getParameterMap() : ", request.getParameterMap().toString());
+//        stringStringHashMap.put("request.getContextPath() :  ", request.getContextPath());
+//        stringStringHashMap.put("request.getHeaderNames().toString()) : ", request.getHeaderNames().toString());
+//        stringStringHashMap.put("ex.toString() : ", ex.toString());
+//        stringStringHashMap.put("ex.getMessage() : ", ex.getMessage());
+//        return new ResponseEntity<>(stringStringHashMap, HttpStatus.FORBIDDEN);
+//
+//    }
+//
+//
+//
+//
+//    @ExceptionHandler
+//    public ResponseEntity<ExceptionResponse> exhandle(NotFoundException e) {
+//        ExceptionResponse errorResult = new ExceptionResponse(HttpStatus.NOT_FOUND.value(),e.getMessage());
+//        return new ResponseEntity<>(errorResult, HttpStatus.NOT_FOUND);
+//    }
+//
+//    @ExceptionHandler
+//    public ResponseEntity<ExceptionResponse> exhandle(IllegalStateException e) {
+//        ExceptionResponse errorResult = new ExceptionResponse(HttpStatus.BAD_REQUEST.value(),e.getMessage());
+//        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
+//    }
+//
+//
+//    // Bean
+//    @Override
+//    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+//                                                                  HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+//        Map<String, Object> body = new LinkedHashMap<>();
+//        body.put("status", status.value());
+//        body.put("message", "값을 제대로 입력해주세요");
+//
+//        Map<String, String> errors = new HashMap<>();
+//        ex.getBindingResult().getAllErrors().forEach((error) -> {
+//            String fieldName = ((FieldError) error).getField();
+//            String errorMessage = error.getDefaultMessage();
+//            errors.put(fieldName, errorMessage);
+//        });
+//        //
+//        log.info("Errors", errors);
+//        return new ResponseEntity<>(body, headers, status);
+//    }
+//
+//
+//
+//}
