6a20d239ccfc09d97b8bbf8edc47afbcd342d671
jacomyou1026 <jacomyou1026@naver.com>
2024-01-27 17:40:38 +0900

    chore : main swagger schema작성

--- src/main/java/com/study/studyproject/list/dto/MainRequest.java
+++ src/main/java/com/study/studyproject/list/dto/MainRequest.java
@@ -2,6 +2,7 @@ package com.study.studyproject.list.dto;
 
 import com.study.studyproject.entity.Category;
 import com.study.studyproject.entity.Recruit;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,11 +15,14 @@ import org.springframework.boot.context.properties.bind.DefaultValue;
 public class MainRequest {
 
     @Nullable
+    @Schema(description = "사용자 이름", defaultValue = "CS")
     Category category;
-
+    
+    @Schema(description = "조회", defaultValue = "0")
     int order; // 0 -> 최신순
 
     @Nullable
+    @Schema(description = "타이틀명", defaultValue = "제목")
     String title;
 
     @Builder
