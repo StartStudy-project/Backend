c3ade51e06f0666c3166136f80ec07f3f9131650
jacomyou1026 <jacomyou1026@naver.com>
2024-07-24 17:17:02 +0900

    적용할 커밋 메시지

--- src/main/java/com/study/studyproject/board/repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.study.studyproject.board.repository;
-
-import com.study.studyproject.entity.Board;
-import com.study.studyproject.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long>  {
-}
