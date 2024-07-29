daede3d398c93326699da2beb05f30f71ae8b156
jacomyou1026 <jacomyou1026@naver.com>
2024-07-29 23:07:01 +0900

    test : Redis에 저장된 데이터를 수정한다.

--- src/test/java/com/study/studyproject/global/config/redis/RedisUtilsTest.java
+++ src/test/java/com/study/studyproject/global/config/redis/RedisUtilsTest.java
@@ -1,12 +1,14 @@
 package com.study.studyproject.global.config.redis;
 
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.Duration;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -18,6 +20,46 @@ class RedisUtilsTest {
 
     @Autowired
     private RedisUtils redisUtils;
+
+    @BeforeEach
+    void shutDown() {
+        redisUtils.setValues(KEY, VALUE, DURATION);
+    }
+
+    @AfterEach
+    void tearDown() {
+        redisUtils.deleteValues(KEY);
+    }
+
+
+    @Test
+    @DisplayName("Redis에 데이터를 저장하면 조회된다")
+    void saveAndFindTest() throws Exception {
+        //given
+        String findValue = redisUtils.getValues(KEY);
+
+        //then
+        assertThat(VALUE).isEqualTo(findValue);
+
+
+    }
+
+    @Test
+    @DisplayName("Redis에 저장된 데이터를 수정한다.")
+    void updateTest() throws Exception {
+        //given
+        String updateValue = "updateValue";
+        redisUtils.setValues(KEY, updateValue, DURATION);
+
+        //when
+        String findValue = redisUtils.getValues(KEY);
+
+        //then
+        assertThat(updateValue).isEqualTo(findValue);
+        assertThat(VALUE).isNotEqualTo(findValue);
+    }
+
+
 
 
 }
