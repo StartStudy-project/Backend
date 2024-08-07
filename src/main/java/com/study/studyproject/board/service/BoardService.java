e91424ac9a4c5d4041457d29842b089312b3a653
jacomyou1026 <jacomyou1026@naver.com>
2024-08-08 01:00:01 +0900

    refactor(): 토큰값에서 memberId를 가져오는 것으로 변경합니다.

--- src/main/java/com/study/studyproject/board/service/BoardService.java
+++ src/main/java/com/study/studyproject/board/service/BoardService.java
@@ -4,17 +4,18 @@ import com.study.studyproject.board.dto.BoardChangeRecruitRequestDto;
 import com.study.studyproject.board.dto.BoardOneResponseDto;
 import com.study.studyproject.board.dto.BoardReUpdateRequestDto;
 import com.study.studyproject.board.dto.BoardWriteRequestDto;
+import com.study.studyproject.entity.Member;
 import com.study.studyproject.entity.Role;
 import com.study.studyproject.global.GlobalResultDto;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public interface BoardService {
-    GlobalResultDto boardSave(BoardWriteRequestDto boardWriteRequestDto, Long idFromToken);
+    GlobalResultDto boardSave(BoardWriteRequestDto boardWriteRequestDto, Long memberId);
 
      GlobalResultDto updateWrite(BoardReUpdateRequestDto boardReUpdateRequestDto);
 
-     BoardOneResponseDto boardOne(Long boardId, String token, HttpServletRequest request, HttpServletResponse response);
+     BoardOneResponseDto boardOne(Long boardId, String memberId, HttpServletRequest request, HttpServletResponse response);
 
 
      GlobalResultDto boardDeleteOne(Long boardId, Role role);
