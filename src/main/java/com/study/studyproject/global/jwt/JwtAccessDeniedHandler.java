678d380c7f564ccb649b91120a0b271d2f3352ad
jacomyou1026 <jacomyou1026@naver.com>
2024-08-15 15:21:33 +0900

    refactor() :  인가 실패 예외 처리 수정 (#50)

--- src/main/java/com/study/studyproject/global/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/study/studyproject/global/jwt/JwtAccessDeniedHandler.java
@@ -1,17 +1,41 @@
 package com.study.studyproject.global.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.studyproject.global.exception.ExceptionResponse;
+import com.study.studyproject.global.exception.ex.ErrorCode;
+import com.study.studyproject.global.exception.ex.ForbiddenException;
+import com.study.studyproject.global.exception.ex.TokenNotValidationException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
 @Component
+@Slf4j
+@RequiredArgsConstructor
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+    private final ObjectMapper objectMapper; // For JSON serialization
+
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
-        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+
+        ExceptionResponse errorResponse = ExceptionResponse.builder()
+                .message(ErrorCode.UNABLE_ACCESS.getMessage())
+                .status(ErrorCode.UNABLE_ACCESS.getStatus().value())
+                .build();
+        // Set response properties
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        String jsonErrorResponse = objectMapper.writeValueAsString(errorResponse);
+        response.getWriter().write(jsonErrorResponse);
     }
 }
