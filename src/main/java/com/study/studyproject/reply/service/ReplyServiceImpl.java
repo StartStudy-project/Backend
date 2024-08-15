4cc10b7f35b01b2d96fdaef45b84f22a57986392
jacomyou1026 <jacomyou1026@naver.com>
2024-08-15 15:31:21 +0900

    refactor ( ReplyServiceImpl): 코드 리펙토링 (#50)

--- src/main/java/com/study/studyproject/reply/service/ReplyServiceImpl.java
+++ src/main/java/com/study/studyproject/reply/service/ReplyServiceImpl.java
@@ -4,9 +4,8 @@ import com.study.studyproject.board.repository.BoardRepository;
 import com.study.studyproject.entity.Board;
 import com.study.studyproject.entity.Member;
 import com.study.studyproject.entity.Reply;
+import com.study.studyproject.global.exception.ex.ErrorCode;
 import com.study.studyproject.global.exception.ex.NotFoundException;
-import com.study.studyproject.global.jwt.JwtUtil;
-import com.study.studyproject.member.repository.MemberRepository;
 import com.study.studyproject.reply.dto.ReplyRequestDto;
 import com.study.studyproject.reply.dto.UpdateReplyRequest;
 import com.study.studyproject.reply.repository.ReplyRepository;
@@ -14,30 +13,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.study.studyproject.global.exception.ex.ErrorCode.*;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class ReplyServiceImpl implements ReplyService {
 
 
-    private final MemberRepository memberRepository;
     private final BoardRepository boardRepository;
     private final ReplyRepository replyRepository;
-    private final JwtUtil jwtUtil;
-
-
 
     @Override
     public void insert(Member member, ReplyRequestDto replyRequestDto) {
         Board board = boardRepository.findById(replyRequestDto.getBoardId())
-                .orElseThrow(() -> new NotFoundException("게시글이 없습니다."));
+                .orElseThrow(() -> new NotFoundException(NOT_FOUND_BOARD));
 
-        ;
         Reply reply = Reply.toEntity(replyRequestDto, board, member);
 
-        if (replyRequestDto.getParentId() != null) { // 대댓글인 경우
+        if (isReplyParent(replyRequestDto)) { // 대댓글인 경우
             Reply replyParent = replyRepository.findById(replyRequestDto.getParentId())
-                    .orElseThrow(() -> new NotFoundException(("댓글을 찾을 수 없습니다.")));
+                    .orElseThrow(() -> new NotFoundException(NOT_FOUND_REPLY));
             reply.updateParent(replyParent);
         }
 
@@ -48,10 +44,14 @@ public class ReplyServiceImpl implements ReplyService {
         replyRepository.save(reply);
     }
 
+    private static boolean isReplyParent(ReplyRequestDto replyRequestDto) {
+        return replyRequestDto.getParentId() != null;
+    }
+
 
     @Override
     public void updateReply(UpdateReplyRequest updateReplyRequest) {
-        Reply findReply = replyRepository.findById(updateReplyRequest.getReplyId()).orElseThrow(() -> new NotFoundException("댓글을 찾을 수 없습니다"));
+        Reply findReply = replyRepository.findById(updateReplyRequest.getReplyId()).orElseThrow(() -> new NotFoundException(NOT_FOUND_REPLY));
         findReply.updateReply(updateReplyRequest.getContent());
     }
 
@@ -59,13 +59,17 @@ public class ReplyServiceImpl implements ReplyService {
     @Override
     public void deleteReply(Long num) { //댓글 num
         Reply reply = replyRepository.findCommentByIdWithParent(num)
-                .orElseThrow(() -> new NotFoundException("댓글을 찾기 못했습니다."));
-        if (reply.getChildren().size() != 0) { //자식이 있는 상태
+                .orElseThrow(() -> new NotFoundException(NOT_FOUND_REPLY));
+        if (hasChildrenReplies(reply)) { //자식이 있는 상태
             reply.ChangeIsDeleted(true);
         } else { //삭제 가능한 조상 댓글
             replyRepository.delete(getDelete(reply));
         }
 
+    }
+
+    private static boolean hasChildrenReplies(Reply reply) {
+        return reply.getChildren().size() != 0;
     }
 
     private Reply getDelete(Reply reply) {
