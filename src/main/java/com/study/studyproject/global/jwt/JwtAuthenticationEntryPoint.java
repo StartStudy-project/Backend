5ccf9f914799f0802842ef4a59a8a706780d98fe
jacomyou1026 <jacomyou1026@naver.com>
2024-08-15 15:21:47 +0900

    pref() :  인증 실패 예외 처리 수정 (#50)

--- src/main/java/com/study/studyproject/global/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/study/studyproject/global/jwt/JwtAuthenticationEntryPoint.java
@@ -1,7 +1,15 @@
 package com.study.studyproject.global.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.studyproject.global.exception.ExceptionResponse;
+import com.study.studyproject.global.exception.ex.ErrorCode;
+import com.study.studyproject.global.exception.ex.ForbiddenException;
+import com.study.studyproject.global.exception.ex.TokenNotValidationException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -9,12 +17,27 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@Slf4j
+@RequiredArgsConstructor
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper; // For JSON serialization
+
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        log.info("--JwtAuthenticationEntryPoint --");
+        ExceptionResponse errorResponse = ExceptionResponse.builder()
+                .message(ErrorCode.TOKEN_EXPIRED.getMessage())
+                .status(ErrorCode.TOKEN_EXPIRED.getStatus().value())
+                .build();
+        // Set response properties
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        String jsonErrorResponse = objectMapper.writeValueAsString(errorResponse);
+        response.getWriter().write(jsonErrorResponse);
 
     }
 }
