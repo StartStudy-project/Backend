b306e43ded51bcbe46661c1a32f1cda4833f0201
jacomyou1026 <jacomyou1026@naver.com>
2024-08-15 15:29:30 +0900

    refactor( ): 예외 코드 수정 (#50)

--- src/main/java/com/study/studyproject/postlike/service/PostLikeService.java
+++ src/main/java/com/study/studyproject/postlike/service/PostLikeService.java
@@ -6,8 +6,8 @@ import com.study.studyproject.entity.Board;
 import com.study.studyproject.entity.Member;
 import com.study.studyproject.entity.PostLike;
 import com.study.studyproject.global.GlobalResultDto;
-import com.study.studyproject.global.exception.ex.UserNotFoundException;
-import com.study.studyproject.member.dto.UserInfoResponseDto;
+import com.study.studyproject.global.exception.ex.ErrorCode;
+import com.study.studyproject.global.exception.ex.NotFoundException;
 import com.study.studyproject.member.repository.MemberRepository;
 import com.study.studyproject.postlike.repository.PostLikeRepository;
 import jakarta.transaction.Transactional;
@@ -16,6 +16,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+
+import static com.study.studyproject.global.exception.ex.ErrorCode.*;
 
 @Service
 @Transactional
@@ -27,7 +29,7 @@ public class PostLikeService {
     private final BoardRepository boardRepository;
 
     public GlobalResultDto postLikeSave(Long boardId ,Member member) {
-        Board board = boardRepository.findById(boardId).orElseThrow(() -> new IllegalArgumentException("게시판이 없습니다."));
+        Board board = boardRepository.findById(boardId).orElseThrow(()-> new NotFoundException(NOT_FOUND_BOARD));
         Optional<PostLike> postLike = postLikeRepository.findByBoardAndMember(board, member);
         if (postLike.isPresent()) {
             return new GlobalResultDto("관심글이 이미 추가하였습니다.", HttpStatus.FORBIDDEN.value());
