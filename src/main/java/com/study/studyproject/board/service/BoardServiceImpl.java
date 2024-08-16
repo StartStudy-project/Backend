2ac5f52e6b09b805476f6220550a4199c1feb5ca
jacomyou1026 <jacomyou1026@naver.com>
2024-08-17 01:52:56 +0900

    refactor(BoardServiceImpl) : 불필요한 부분 삭제 ( #53)

--- src/main/java/com/study/studyproject/board/service/BoardServiceImpl.java
+++ src/main/java/com/study/studyproject/board/service/BoardServiceImpl.java
@@ -41,7 +41,6 @@ public class BoardServiceImpl implements BoardService{
     private final PostLikeRepository postLikeRepository;
     private final JwtUtil jwtUtil;
 
-    private final static String VIEWCOOKIENAME = "alreadyViewCookie";
 
     //작성
     @Override
@@ -60,19 +59,7 @@ public class BoardServiceImpl implements BoardService{
         board.updateBoard(boardReUpdateRequestDto);
         return new GlobalResultDto("글 작성 완료", HttpStatus.OK.value());
     }
-    private Cookie createCookieForForNotOverlap(Long postId) {
-        Cookie cookie = new Cookie(VIEWCOOKIENAME + postId, String.valueOf(postId));
-        cookie.setMaxAge(getRemainSecondForTommorow());
-        cookie.setHttpOnly(true);
-        return cookie;
-    }
 
-    // 다음 날 정각까지 남은 시간(초)
-    private int getRemainSecondForTommorow() {
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime tommorow = LocalDateTime.now().plusDays(1L).truncatedTo(ChronoUnit.DAYS);
-        return (int) now.until(tommorow, ChronoUnit.SECONDS);
-    }
 
 
     //글 1개만 가져오기
@@ -81,7 +68,6 @@ public class BoardServiceImpl implements BoardService{
 
         Board board = boardRepository.findById(boardId).orElseThrow(()->new NotFoundException(NOT_FOUND_BOARD));
 
-        checkDuplicate(boardId, request, response, board);
         Long currentMemberId = 0L;
 
 
@@ -102,26 +88,6 @@ public class BoardServiceImpl implements BoardService{
 
     }
 
-    private void checkDuplicate(Long boardId, HttpServletRequest request, HttpServletResponse response, Board board) {
-        Cookie[] cookies = request.getCookies();
-        boolean checkCookie = false;
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                // 이미 조회를 한 경우 체크
-                if (cookie.getName().equals(VIEWCOOKIENAME + boardId)) checkCookie = true;
-
-            }
-            if (!checkCookie) {
-                Cookie newCookie = createCookieForForNotOverlap(boardId);
-                response.addCookie(newCookie);
-                board.updateViewCnt(board.getViewCount());
-            }
-        } else {
-            Cookie newCookie = createCookieForForNotOverlap(boardId);
-            response.addCookie(newCookie);
-            board.updateViewCnt(board.getViewCount());
-        }
-    }
 
 
     private ReplyResponseDto findReplies(Long boardId,Long currentMemberId) {
@@ -152,8 +118,7 @@ public class BoardServiceImpl implements BoardService{
     public GlobalResultDto boardDeleteOne(Long boardId, Role role) {
         if (isAdmin(role)) {
             Board board = boardRepository.findById(boardId).orElseThrow(()->new NotFoundException(NOT_FOUND_BOARD) );
-            board.ChangeBoardIsDeleted(true);
-            board.deleteBoardContent("관리자로 의해 게시글 삭제","관리자로 의해 게시글 삭제되었습니다.");
+            board.changeAdminDeleteBoard();
             return new GlobalResultDto("관리자 권한으로 게시글 삭제 완료", HttpStatus.OK.value());
         }
 
@@ -189,9 +154,9 @@ public class BoardServiceImpl implements BoardService{
 
     //모집 구분 변경
     @Override
-    public GlobalResultDto changeRecruit(BoardChangeRecruitRequestDto boardChangeRecruitRequestDto) {
-        Board board = boardRepository.findById(boardChangeRecruitRequestDto.getBoardId() ).orElseThrow(() -> new NotFoundException(NOT_FOUND_BOARD));
-        board.changeRecuritBoard(boardChangeRecruitRequestDto);
+    public GlobalResultDto changeRecruit(Long boardId) {
+        Board board = boardRepository.findById(boardId).orElseThrow(() -> new NotFoundException(NOT_FOUND_BOARD));
+        board.changeRecuritBoard();
         return new GlobalResultDto("모집 구분 변경 완료", HttpStatus.OK.value());
     }
 }
