05cb457d08d704e52eab19e4ca36e8a6a4c0e419
jacomyou1026 <jacomyou1026@naver.com>
2024-08-20 16:30:10 +0900

    refactor(BoardService) : 게시글 상세 페이지 파라미터 수정  (#53)

--- src/main/java/com/study/studyproject/board/service/BoardService.java
+++ src/main/java/com/study/studyproject/board/service/BoardService.java
@@ -3,18 +3,16 @@ package com.study.studyproject.board.service;
 import com.study.studyproject.board.dto.BoardOneResponseDto;
 import com.study.studyproject.board.dto.BoardReUpdateRequestDto;
 import com.study.studyproject.board.dto.BoardWriteRequestDto;
-import com.study.studyproject.entity.Member;
 import com.study.studyproject.entity.Role;
 import com.study.studyproject.global.GlobalResultDto;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import com.study.studyproject.global.auth.UserDetailsImpl;
 
 public interface BoardService {
     GlobalResultDto boardSave(BoardWriteRequestDto boardWriteRequestDto, Long memberId);
 
      GlobalResultDto updateWrite(BoardReUpdateRequestDto boardReUpdateRequestDto);
 
-     BoardOneResponseDto boardOne(Long boardId, String memberId, HttpServletRequest request, HttpServletResponse response);
+     BoardOneResponseDto boardOne(Long boardId, UserDetailsImpl userDetails);
 
 
      GlobalResultDto boardDeleteOne(Long boardId, Role role);
