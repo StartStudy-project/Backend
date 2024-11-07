a8ca2a364071a6d6dfd9296e1aa08b4e38e4d35f
jacomyou1026 <jacomyou1026@naver.com>
2024-11-07 22:50:31 +0900

    refactor : #79 재발급 토큰들 header에 추가

--- src/main/java/com/study/studyproject/login/service/LoginService.java
+++ src/main/java/com/study/studyproject/login/service/LoginService.java
@@ -105,6 +105,7 @@ public class LoginService {
             RefreshToken getRefreshToken = refreshRepository.findById(emailFromToken).orElseThrow(() -> new TokenNotValidationException(INVALID_REFRESH_TOKEN));
             String renewAccessToken = jwtUtil.createToken(emailFromToken, idFromToken, ACCESS_TOKEN); //엑세스 토큰 재생성
             getRefreshToken.updateAccessToken(renewAccessToken); // 갱신
+
             setHeader(response, TokenDtoResponse.of(renewAccessToken, refreshToken));
             return renewAccessToken;
 
@@ -113,6 +114,7 @@ public class LoginService {
         //AT 문제가 둘다 없는 경우
         if (jwtUtil.isValidRefreshAndValidAccess(accessToken, refreshToken)) {
             log.info("둘다 문제 없는 경우");
+            setHeader(response, TokenDtoResponse.of(accessToken, refreshToken));
             return accessToken;
         }
 
