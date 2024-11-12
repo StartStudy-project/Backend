12d7fa4e181d226ae9b246955fba8e1c2c01d5fa
jacomyou1026 <jacomyou1026@naver.com>
2024-11-12 13:24:30 +0900

    feat :#77 OAuth2LoginFailureHandler 추가

--- src/main/java/com/study/studyproject/global/oauth/handler/OAuth2LoginFailureHandler.java
+++ src/main/java/com/study/studyproject/global/oauth/handler/OAuth2LoginFailureHandler.java
@@ -3,10 +3,13 @@ package com.study.studyproject.global.oauth.handler;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 
@@ -14,14 +17,23 @@ import static jakarta.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
+
+    @Value("${location}")
+    private  String location;
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        response.getWriter().write("소셜 로그인 실패하였습니다.");
-        response.setContentType("application/json");
+        response.setContentType("application/json; charset=UTF-8");
         response.setCharacterEncoding("UTF-8");
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(location);
+        String redirectionUri = uriBuilder
+                .queryParam("loginSuccess", false)
+                .build()
+                .toUriString();
+        response.sendRedirect(redirectionUri);
 
         log.error("소셜 로그인에 실패했습니다. 에러 메시지 : {}", exception.getMessage());
     }
